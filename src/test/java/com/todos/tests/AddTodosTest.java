4afbc2444f741950446da321d2522d820b8f8235
Inesmatmati <matmatiines219@gmail.com>
2025-04-04 20:51:56 +0200

    Selenium TestNG

--- src/test/java/com/todos/tests/AddTodosTest.java
+++ src/test/java/com/todos/tests/AddTodosTest.java
@@ -1,5 +1,34 @@
 package com.todos.tests;
 
-public class AddTodosTest {
+import java.io.IOException;
 
+import org.junit.jupiter.api.Assertions;
+import org.testng.annotations.Test;
+
+import com.todos.pages.TodoPages;
+import com.todos.utils.Setup;
+
+public class AddTodosTest extends Setup {
+
+	public AddTodosTest() throws IOException {
+		super();
+
+	}
+
+	TodoPages todopage;
+
+	@Test
+	public void iAddToDo() throws IOException {
+
+		todopage = new TodoPages();
+		boolean fieldToDo = todopage.isElementDisplayed(TodoPages.inputField);
+		Assertions.assertTrue(fieldToDo);
+		todopage.fillTodo(prop.getProperty("todo1"));
+
+		String getText = todopage.checkElementContain(TodoPages.textToDo);
+		Assertions.assertTrue(getText.contains(prop.getProperty("todo1")));
+
+		boolean checkbox = todopage.isChekboxSelected(TodoPages.checkboxToDo);
+		Assertions.assertFalse(checkbox);
+	}
 }
